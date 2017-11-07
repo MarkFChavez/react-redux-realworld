74c39e3668cbab5ee39b0355a8c42ba0f17ff029
Mark Chavez <markchav3z@gmail.com>
2017-11-08 07:21:29 +0800

    Setup dotenv

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// setup dotenv
+import { config } from 'dotenv';
+config();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
