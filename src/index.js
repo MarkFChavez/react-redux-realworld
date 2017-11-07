18942954e801adcecdb5b756a11b1c29031a9ad4
Mark Chavez <markchav3z@gmail.com>
2017-11-08 07:32:31 +0800

    Setup redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+
+// redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 // setup dotenv
 import { config } from 'dotenv';
 config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
+
 registerServiceWorker();
