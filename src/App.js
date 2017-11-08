607d838a3f9753aefd455e87e0f04fa789b65e73
Mark Chavez <markchav3z@gmail.com>
2017-11-08 15:55:33 +0800

    add jsdoc

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { Route, Switch } from 'react-router-dom'
 const Home = () => <div> Home </div>
 const About = () => <div> About </div>
 
+/**
+ * @param '/' {Go to Article List}
+ * @param '/articles/:slug' {Go to Article Page}
+ */
 const App = () => (
   <div className="App">
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
+      <Route exact path="/articles/:slug" component={About} />
     </Switch>
   </div>
 )
