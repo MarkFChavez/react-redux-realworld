749c99144f0b42fa2ccdaa2c7c5c17ed2b6c6897
Mark Chavez <markchav3z@gmail.com>
2017-11-15 19:38:28 +0800

    add parent container on article item

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -4,14 +4,16 @@ import { markdown } from 'markdown'
 const ArticleItemPage = ({ article }) => {
   const innerHtml = { __html: markdown.toHTML(article.body) }
   return (
-    <div className='ui segment'>
-      <div className='ui huge header'>{article.title}</div>
-      <p dangerouslySetInnerHTML={innerHtml}>
-      </p>
-      <div>
-        <img alt='' src={article.author.image} width={32} height={32} />
-        &nbsp;
-        created by <a>{article.author.username}</a> {article.createdAt}
+    <div className='article--item'>
+      <div className='ui segment'>
+        <div className='ui huge header'>{article.title}</div>
+        <p dangerouslySetInnerHTML={innerHtml}>
+        </p>
+        <div>
+          <img alt='' src={article.author.image} width={32} height={32} />
+          &nbsp;
+          created by <a>{article.author.username}</a> {article.createdAt}
+        </div>
       </div>
     </div>
   )
