f7013b6bc6b82a0712b28d85442b9f42c454b1a7
Mark Chavez <markchav3z@gmail.com>
2017-11-18 09:15:41 +0800

    Remove unused Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Header from './components/commons/Header'
 import ArticlesContainer from './containers/ArticlesContainer'
