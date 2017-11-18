af7546a743537ac9e36752efe23d53083d1fbdb2
Mark Chavez <markchav3z@gmail.com>
2017-11-18 17:19:21 +0800

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import store from './store/configureStore'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
-import store from './store/configureStore'
 import { ConnectedRouter } from 'react-router-redux'
-import { config } from 'dotenv'
 import { history } from './history'
 import 'semantic-ui-css/semantic.css'
 import './assets/css/styles.css'
 
 // Enables us to store different environment variables which can be
 // accessed by calling `process.env`
+import { config } from 'dotenv'
 config()
 
 render(
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 import { middleware } from '../history';
+import {persistStore, autoRehydrate} from 'redux-persist'
 
-export default createStore(
+const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(middleware, thunk)
+  compose(
+    applyMiddleware(middleware, thunk),
+    autoRehydrate()
+  )
 )
+
+persistStore(store)
+
+export default store
