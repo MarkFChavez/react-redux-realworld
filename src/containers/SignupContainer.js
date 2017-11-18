b62360629f2bdd537d1ebd6d1785c12145a3b910
Mark Chavez <markchav3z@gmail.com>
2017-11-18 09:40:45 +0800

    UI for sign out

--- src/containers/SignupContainer.js
+++ src/containers/SignupContainer.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import Form from '../components/commons/Form'
+import Field from '../components/commons/FormField'
 
 class SignupContainer extends Component {
 
+  constructor (props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+    console.log('submitted')
+  }
+
   render () {
-    return <div> sign up here </div>
+    return (
+      <div className='ui text container'>
+        <div className='ui segment'>
+          <center>
+            <h1>
+              <i className='code icon'></i> Sign up
+            </h1>
+          </center>
+
+          <Form onSubmit={this.onSubmit}>
+            <Field name='email' type='text' placeholder='Email address' icon='mail' />
+            <Field name='password' type='password' placeholder='Your password' icon='lock' />
+            <Field name='confirm_password' type='password' placeholder='Confirm your password' icon='lock' />
+
+            <button className='ui green huge button' type='submit'>Submit</button>
+          </Form>
+        </div>
+      </div>
+    )
   }
 
 }
