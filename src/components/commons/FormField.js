72f2fdcc9d3434116ea2c5cf11135c547c12d0b8
Mark Chavez <markchav3z@gmail.com>
2017-11-18 09:47:12 +0800

    add string sanitize

--- src/components/commons/FormField.js
+++ src/components/commons/FormField.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { capitalize } from 'lodash'
+import { capitalize, replace } from 'lodash'
+
+const sanitize = (string, target, term) => replace(capitalize(string), target, term)
 
 const FormField = props => (
   <div className='field'>
     <label>
-      <i className={`${props.icon} icon`}></i> {capitalize(props.name)}
+      <i className={`${props.icon} icon`}></i> {sanitize(props.name, '_', ' ')}
     </label>
     <input type={props.type} name={props.name} placeholder={props.placeholder} />
   </div>
