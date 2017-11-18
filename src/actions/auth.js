69b4ee22be43b1d54e752576c4ad8e0dd7430710
Mark Chavez <markchav3z@gmail.com>
2017-11-18 16:38:59 +0800

    redirect to / after login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 import * as types from '../constants/actionTypes'
 import api from '../api'
+import { push } from 'react-router-redux'
 
 export const signin = (email, password) => {
   return dispatch => {
@@ -7,6 +8,7 @@ export const signin = (email, password) => {
     api.Auth.signin(email, password)
       .then(response => {
         dispatch({ type: types.AUTH_SUCCESS, payload: response.data.user })
+        dispatch(push('/'))
       })
       .catch(error => {
         dispatch({ type: types.AUTH_FAILED })
