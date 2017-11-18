5f00406664c42b4a5566a32ca4dbf4e326ab1338
Mark Chavez <markchav3z@gmail.com>
2017-11-18 17:33:11 +0800

    remove redux persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,17 +2,11 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 import { middleware } from '../history';
-import {persistStore, autoRehydrate} from 'redux-persist'
 
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  compose(
-    applyMiddleware(middleware, thunk),
-    autoRehydrate()
-  )
+  applyMiddleware(middleware, thunk),
 )
-
-persistStore(store)
 
 export default store
